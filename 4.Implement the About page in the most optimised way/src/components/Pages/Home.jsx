27be37c39e5b9c67baf6a5911f2b0dbc8382561a
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-04-24 19:09:21 +0530

    4.Implement the About page in the most optimised way

---
+++ 4.Implement the About page in the most optimised way/src/components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Layout/Header  no cart";
+import Footer from "../Layout/Footer";
+import "./Home.css";
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
