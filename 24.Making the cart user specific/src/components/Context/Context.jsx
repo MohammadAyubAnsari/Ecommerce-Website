c5d23608e8e8f43c1a0ecdff9ea2d4c9b5236199
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-05-08 22:30:44 +0530

    24.Making the cart user specific

---
+++ 24.Making the cart user specific/src/components/Context/Context.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+const CartContext = React.createContext({
+  productlist: [],
+  cartOpen: false,
+  setCartOpen: () => {},
+  orderList: [],
+  setOrderList: () => {},
+  idToken: "",
+  setIdToken: () => {},
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
+  signInModalVisibility: false,
+  setSignInModalVisibility: () => {},
+  userid: "",
+  setUserId: () => {},
+});
+
+export default CartContext;
